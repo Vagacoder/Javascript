e584ba936ad565c1545768a6b19f8ebdb679bca3
Vagacoder <qiruihu@gmail.com>
2020-04-21 23:47:45 -0700

    4/21 still working on ch6 regex

--- es6/src/009-regex.js
+++ es6/src/009-regex.js
@@ -61,3 +61,35 @@ console.log(r3.exec(s1));
 console.log(r1.exec(s1));
 console.log(r2.exec(s1));
 console.log(r3.exec(s1));
+
+// ? 2.2. for string methods, y flag must be used with g
+console.log('\n2.2. for string method, using y with g flag to get all matches ...');
+console.log('a1a2a3'.match(/a\d/y));
+console.log('a1a2a3'.match(/a\d/yg));
+
+// ? Ex.3 s flag for dotAll mode
+console.log('\n3.1. dotAll mode for s flag ...');
+let s2 = 'foo\nbar';
+console.log(s2.match(/foo.bar/));
+console.log(s2.match(/foo[^]bar/));
+console.log(s2.match(/foo.bar/s));
+
+// ? Ex.4 lookbehind and negative lookbehind
+// ? 4.1. general using
+console.log('\n4.1. lookbehind ....');
+console.log(/(?<=\$)\d+/.exec('Benjamin Franklin is on the $100 bill.'));
+console.log(/(?<!\$)\d+/.exec('It is worth €23.'));
+
+// ? 4.2. Note the difference
+console.log('\n4.2. Note the difference ....');
+console.log(/(?<=(\d+)(\d+))$/.exec('1053'));
+console.log(/$/.exec('1053'));
+console.log(/(?<=(\d+))(\d+)$/.exec('1053'));
+console.log(/(?<=(\d+))(\d+?)$/.exec('1053'));
+console.log(/^(\d+)(\d+)$/.exec('1053'));
+
+// ? 4.3. Note the position of the quotation (\1)
+// ? the evaluation order of lookbehind is from right to left
+console.log('\n4.3. Note the position of \\1');
+console.log(/(?<=\1d(o))r/.exec('hodor'));
+console.log(/(?<=(o)d\1)r/.exec('hodor'));
