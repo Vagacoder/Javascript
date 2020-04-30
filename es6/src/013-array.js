6d01c17946a71b6e0d70efc85a4d37ee49b57428
Vagacoder <qiruihu@gmail.com>
2020-04-30 13:33:13 -0700

    4/30 ch9 array is done

--- es6/src/013-array.js
+++ es6/src/013-array.js
@@ -52,3 +52,82 @@ let str1 = 'x\uD83D\uDE80y';
 console.log(str1);
 console.log(str1.length);
 console.log([...str1].length);
+
+// ? Ex.2 Array.form()
+// ? 2.1. convert array-like object to real array
+console.log('\2.1. Array.form() convert array-like object to a real array ...');
+let arrlike = {
+    '0': 'a',
+    '1': 'b',
+    '2': 'c',
+    'length': 3
+}
+console.log(arrlike);
+console.log([].slice.call(arrlike));
+console.log(Array.from(arrlike));
+console.log(Array.from({length: 3}));
+
+// ? 2.2 spread syntax ... can NOT convert array-like object
+console.log('\n2.2. Spread syntax ... can not convert array-like object ...');
+console.log(arrlike);
+// ! console.log([...arrlike]); // error!
+// ! console.log([...{length: 3}]); // error!
+
+// ? 2.3. Array.form conver iterable object
+console.log('\n\2.3. Array.form() convert iterable object ... ');
+console.log(Array.from("Hello, world!"));
+
+// ? 2.4. Array.form using 2nd parameter
+console.log('\n2.4. Array.form() 2nd parameter ...');
+console.log(arrlike);
+console.log(Array.from(arrlike, (x, index)=> (index + ": " + x).toUpperCase()));
+console.log(Array.from({length: 10}, x => -1));
+
+
+// ? Ex.3 Array.of()
+// ? 3.1. Array.of() can replace array constructor
+console.log('\n3.1. Array.of() for arry constructor ...');
+console.log(Array.of());
+console.log(Array.of(5));
+console.log(Array.of(5, 10, 15));
+
+// ? Ex.4 Array.prototype.copyWithin()
+// ? 4.1. simple examples
+console.log('\n4.1. Array.prototype.copyWithin() ...');
+let arr7 = [0, 1, 2 ,3, 4, 5, 6];
+let arr8 = [...arr7];
+console.log(arr7);
+console.log(arr7.copyWithin(1, 3, 5));
+console.log(arr7.copyWithin(1,3));
+
+// ? Ex.5 Array.prototype.find() and findIndex()
+// ? 5.1. simple examples
+console.log('\n5.1. Array.prototype.find() and findIndex() ... ');
+console.log(arr8);
+console.log(arr8.find((n, index, arr)=>{return (n*n - index*2*2) > 0}));
+console.log(arr8.findIndex((n, index, arr)=>{return (n*n - index*2*2) > 0}));
+
+// ? Ex.6 fill()
+// ? 6.1. simple example
+console.log('\n6.1. fill() ... ');
+let arr9 = new Array(10);
+console.log(arr9);
+arr9.fill(119, 0, 10);
+console.log(arr9);
+
+// ? Ex.7 flat() and flatMap()
+// ? 7.1. simple example
+console.log('\n7.1. flat() ... ');
+let arr10 = [1, 2, [3, 4]];
+console.log(arr10);
+console.log(arr10.flat());
+let arr11 = [1, [2, [3, [4, 5]]]];
+console.log(arr11);
+console.log(arr11.flat());
+console.log(arr11.flat(2));
+console.log(arr11.flat(Infinity));
+
+
+console.log('a' in {'a': 1, 'b':2, 'c':3});
+console.log('a' in ['a', 'b', 'c']);
+console.log(0 in ['a', 'b', 'c']);
